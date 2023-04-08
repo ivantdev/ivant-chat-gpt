06d2ac00dca817157457a634dcd50f34b637a4e9
Ivan Castellanos <ivan@ivant.dev>
2023-04-07 22:25:46 -0500

    tailwind css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        chat_bg_primary: "#f8fefe",
+        chat_bg_secondary: "#d7ebf4",
+        chat_green: "#005568",
+      },
+      fontFamily : {
+        "inter": ["Inter", "sans-serif"],
+      }
+    }
   },
   plugins: [],
 }
