9acecf187a47ea3f08efe5f8ec501279e7d6bc66
Ivan Castellanos <ivan@ivant.dev>
2023-04-09 20:53:37 -0500

    styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,8 @@ module.exports = {
         chat_bg_primary: "#f8fefe",
         chat_bg_secondary: "#d7ebf4",
         chat_green: "#005568",
+        chat_bg_primary_alpha: "#d7ebf48f",
+        chat_bg_secondary_alpha: "#f8fefe8f",
       }
     }
   },
